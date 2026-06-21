635812ca4a9249781b9119365c14d57a65aeb1e4
Shivam Mahalle <shivammahalle41@gmail.com>
2026-06-21 15:35:57 +0530

    update resume

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -49,7 +49,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1O2CPl2E5vZ6eweZhtU3Jdj9-_dUnJZ1-/view?usp=sharing"
+            href="https://drive.google.com/file/d/1cvYgabjH4QEUbZI26POmX6VQ0X9svq6K/view?usp=drive_link"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
